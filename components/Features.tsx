060742d860035287c565768fa0670a46ea0999a3
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-07-01 22:08:20 -0700

    Updates to th Features Component

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import Image from "next/image";
+
+interface FeaturesProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+const FeaturesData = ({ icon, title, description }: FeaturesProps) => (
+  <div>
+    <div>
+      <Image src={icon} alt="map" height={28} width={28} />
+    </div>
+    <h4>{title}</h4>
+    <p>{description}</p>
+  </div>
+);
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="flex flex-row padding-container max-container w-full pb-24">
+      {/* Image Section */}
+      {/* <div className="flex">
+        <Image
+          className="relative"
+          src="/feature-bg.png"
+          alt="background"
+          height={450}
+          width={850}
+        />
+        <Image
+          className="absolute z-20 origin-top-left rotate-12"
+          src="/phone.png"
+          alt="phone"
+          height={150}
+          width={410}
+        />
+      </div> */}
+      {/* Features Section */}
+      <div>
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Our Features</h2>
+        <div>
+          <FeaturesData
+            icon="/play.svg"
+            title="Real Maps Can Be Offline"
+            description="We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
