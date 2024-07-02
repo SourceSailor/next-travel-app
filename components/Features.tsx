575bf822ab701d18a28db0615b01a6bf63244653
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-07-02 08:38:41 -0700

    Finalization of Features Component

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FEATURES } from "../constants";
 
 interface FeaturesProps {
   icon: string;
@@ -7,45 +8,58 @@ interface FeaturesProps {
 }
 
 const FeaturesData = ({ icon, title, description }: FeaturesProps) => (
-  <div>
-    <div>
-      <Image src={icon} alt="map" height={28} width={28} />
+  <li className="flex w-full flex-1 flex-col items-start">
+    <div className="rounded-full p-4 lg:p-7 bg-green-50">
+      <Image src={icon} alt="map" width={28} height={28} />
     </div>
-    <h4>{title}</h4>
-    <p>{description}</p>
-  </div>
+    <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+    <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt[30px] lg:bg-none">
+      {description}
+    </p>
+  </li>
 );
 
 const Features = () => {
   return (
-    <section className="flex flex-row padding-container max-container w-full pb-24">
+    <section className="flex-col flexCenter overflow-hidden  bg-feature-bg bg-center bg-no-repeat py-24">
       {/* Image Section */}
-      {/* <div className="flex">
-        <Image
-          className="relative"
-          src="/feature-bg.png"
-          alt="background"
-          height={450}
-          width={850}
-        />
-        <Image
-          className="absolute z-20 origin-top-left rotate-12"
-          src="/phone.png"
-          alt="phone"
-          height={150}
-          width={410}
-        />
-      </div> */}
-      {/* Features Section */}
-      <div>
-        <Image src="/camp.svg" alt="camp" width={50} height={50} />
-        <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Our Features</h2>
-        <div>
-          <FeaturesData
-            icon="/play.svg"
-            title="Real Maps Can Be Offline"
-            description="We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location"
+      <div className="flex max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            className="feature-phone"
+            src="/phone.png"
+            alt="phone"
+            height={150}
+            width={410}
           />
+        </div>
+
+        {/* Features Section */}
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              className="absolute left-[-5px] top-[-35px] lg:w-[50px]"
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+            />
+            <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+              Our Features
+            </h2>
+          </div>
+          <div>
+            <ul className="mt-10 grid gap-10 md:grid-col-2 lg:mt-20 lg:gap-20">
+              {FEATURES.map((feature) => (
+                <FeaturesData
+                  key={feature.title}
+                  title={feature.title}
+                  description={feature.description}
+                  icon={feature.icon}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </section>
