cf74e4a669e8df2d676f91affe97072305caa590
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-07-08 20:08:19 -0700

    Update to Footer Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,22 @@
 import { FOOTER_LINKS, FOOTER_CONTACT_INFO, SOCIALS } from "../constants";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   const firstFooterLink = FOOTER_LINKS[0];
   const secondFooterLink = FOOTER_LINKS[1];
   return (
     <footer className="flexCenter flex-col mb-24">
-      <div className="padding-container max-container flex w-full flex-row gap-14">
+      <div className="padding-container max-container flex w-full flex-row gap-14 flex-wrap md:flex-1 mb-10">
         {/* Logo */}
-        <div>
-          <Image src="/hilink-logo.svg" alt="logo" height={100} width={100} />
+        <div className="flex flex-col items-start justify-center flex-1">
+          <Link href="/">
+            <Image src="/hilink-logo.svg" alt="logo" height={100} width={100} />
+          </Link>
         </div>
 
         {/* Learn More */}
-        <div>
+        <div className="md:flex-1">
           <h4 className="text-gray-60 bold-18">{firstFooterLink.title}</h4>
           <ul>
             {firstFooterLink.links.map((link, i) => (
@@ -23,9 +26,9 @@ const Footer = () => {
         </div>
 
         {/* Our Community */}
-        <div>
+        <div className="md:flex-1">
           <h4 className="text-gray-60 bold-18">{secondFooterLink.title}</h4>
-          <ul>
+          <ul className="regular-14 flex flex-col gap-1 text-gray-40">
             {secondFooterLink.links.map((link, i) => (
               <li key={i}>{link}</li>
             ))}
@@ -33,9 +36,9 @@ const Footer = () => {
         </div>
 
         {/* Contact Us */}
-        <div>
+        <div className="md:flex-1">
           <h4 className="text-gray-60 bold-18">{FOOTER_CONTACT_INFO.title}</h4>
-          <ul>
+          <ul className="regular-14 flex flex-col gap-1 text-gray-40">
             {FOOTER_CONTACT_INFO.links.map((link, i) => (
               <li key={i}>
                 {link.label}: {link.value}
@@ -45,9 +48,9 @@ const Footer = () => {
         </div>
 
         {/* Socials */}
-        <div>
+        <div className="md:flex-1">
           <h4 className="text-gray-60 bold-18">{SOCIALS.title}</h4>
-          <ul className="flex flex-row gap-3">
+          <ul className="flex md:flex-col lg:flex-row gap-3">
             {SOCIALS.links.map((link, i) => (
               <li key={i}>
                 <Image src={link} alt="socials" height={25} width={25} />
@@ -56,8 +59,8 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="border-t-4 border-indigo-500 ">
-        <p>2023 Hilink | All rights reserved</p>
+      <div className="border-t-2 border-gray-20 w-[80%] text-center mx-10">
+        <p className="pt-8">2023 Hilink | All rights reserved</p>
       </div>
     </footer>
   );
